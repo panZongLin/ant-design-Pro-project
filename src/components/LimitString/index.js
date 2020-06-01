c65fab17d220ec748ca9105e927eb0d6f2fd138f
zonglinpan <1328785074@qq.com>
2020-06-01 16:59:03 +0800

    fff

---
+++ src/components/LimitString/index.js
@@ -0,0 +1,3 @@
+import LimitString from './LimitString'
+
+export default LimitString
---
+++ src/models/login.js
@@ -0,0 +1,68 @@
+import { notification } from 'antd';
+import { stringify } from 'querystring';
+import { history } from 'umi';
+import { fakeAccountLogin } from '@/services/login';
+import { setAuthority } from '@/utils/authority';
+import { getPageQuery } from '@/utils/utils';
+
+const Model = {
+  namespace: 'login',
+  state: {
+    status: undefined,
+  },
+  effects: {
+    *login({ payload }, { call, put }) {
+      const response = yield call(fakeAccountLogin, payload);
+
+      if(response.code > 0) {
+        //储存
+        yield put({
+          type: 'changeLoginStatus',
+          payload: response.data,
+        }); // Login successfully
+  
+        //跳转
+        const urlParams = new URL(window.location.href);
+        const params = getPageQuery();
+        let { redirect } = params;
+        if (redirect) {
+          const redirectUrlParams = new URL(redirect);
+          if (redirectUrlParams.origin === urlParams.origin) {
+            redirect = redirect.substr(urlParams.origin.length);
+
+            if (redirect.match(/^\/.*#/)) {
+              redirect = redirect.substr(redirect.indexOf('#') + 1);
+            }
+          } else {
+            window.location.href = '/';
+            return;
+          }
+        }
+        history.replace(redirect || '/');
+
+      }     
+    },
+
+    logout() {
+      localStorage.removeItem('token');
+      localStorage.removeItem('userType');
+      const { redirect } = getPageQuery(); // Note: There may be security issues, please note
+
+      if (window.location.pathname !== '/user/login' && !redirect) {
+        history.replace({
+          pathname: '/user/login',
+          search: stringify({
+            redirect: window.location.href,
+          }),
+        });
+      }
+    },
+  },
+  reducers: {
+    changeLoginStatus(state, { payload }) {
+      setAuthority(payload);
+      return { ...state, status: payload.status, type: payload.type };
+    },
+  },
+};
+export default Model;
